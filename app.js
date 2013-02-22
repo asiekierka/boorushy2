f9e7e6271431c898e50c7fbfa195d9bf6db607ec
asiekierka <asiekierka@gmail.com>
2013-02-22 13:00:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,128 @@
+var express = require('express')
+  , mkdirp = require('mkdirp')
+  , _ = require('underscore')
+  , fs = require('fs')
+  , JSON = require('JSON2')
+  , imageDB = require('./imagedb.js').ImageDB
+  , im = require('imagemagick')
+  , program = require('commander')
+  , path = require('path')
+  , qs = require('querystring');
+
+var app = express()
+  , config = require('./config.json')
+  , templates = {};
+
+var defaultImage = { author: "Unknown", source: "/", uploader: "Computer" };
+
+_.each(fs.readdirSync("templates/"),function(filename) {
+  var name = filename.split(".")[0];
+  console.log("[Template] Loading template "+name);
+  templates[name] = fs.readFileSync("templates/"+filename,"utf8");    
+});
+
+function fileExt(name) {
+  var ext = path.extname(name).split(".");
+  return ext[ext.length-1];
+}
+
+var thumbW = 300, thumbH = 300;
+
+function resize(src,dest,w,h) {
+  var cnf = { srcPath: src, dstPath: dest,
+              width: w, height: h+"^",
+              customArgs: [ "-gravity", "center", "-extent", w+"x"+h ] };
+  im.resize(cnf,null);
+}
+
+function thumbnail(src,dest,dest2x) {
+  resize(src,dest,thumbW,thumbH);
+  if(_.isString(dest2x))
+    resize(src,dest2x,thumbW*2,thumbH*2);
+}
+
+function addImage(rawdata,format,info) {
+  imageDB.count("imageId",function(err,id) {
+    var path = id+"."+format;
+    fs.writeFile("img/src/"+path,rawdata,"utf8",function() {
+      im.identify("img/src/"+path, function(err,features) {
+        if(err) throw err;
+        var data = { id: id, format: format, filename: path, originalName: info.filename,
+                     width: features.width, height: features.height,
+                     tags: info.tags, uploader: info.uploader, author: info.author};
+        imageDB.set(id,_.defaults(data,defaultImage));
+      });
+      thumbnail("img/src/"+path,"img/thumb/"+path,"img/thumb2x/"+path);
+    });
+  });  
+}
+
+function makeRawTemplate(name,conf,noHeader) {
+  var conf2 = _.defaults(conf,config);
+  var body = _.template(templates[name],conf2);
+  if(!noHeader)
+    body = _.template(templates["header"],conf2) + body;
+  return body;
+}
+function makeTemplate(name,conf,raw,noHeader) {
+  if(raw=="raw") return makeRawTemplate(name,conf,noHeader);
+  var conf2 = _.defaults(conf,config);
+  return _.template(templates["main"],_.defaults(conf2,{page: makeRawTemplate(name,conf,noHeader)}));
+}
+
+// Create missing directories (just in case)
+mkdirp.sync("img/src");
+mkdirp.sync("img/thumb");
+mkdirp.sync("img/thumb2x");
+
+// Load directory dependencies
+app.use("/static/",express.static("bootstrap"));
+app.use("/static/",express.static("static"));
+app.use("/img/",express.static("img"));
+app.use("/img/thumb/", function(req,res) { res.redirect("/static/img/thumbnotfound.png"); });
+app.use("/img/thumb2x/", function(req,res) { res.redirect("/static/img/thumbnotfound.png"); });
+app.use("/upload/",express.basicAuth("test","test"));
+app.use("/upload/",function(req,res) {
+  var p = qs.unescape(req.path).split("/");
+  res.send(makeTemplate("upload",{title: "Boorushy Upload"},p[1]));
+});
+app.use("/image/",function(req,res) {
+  var p = qs.unescape(req.path).split("/");
+  if(!p[1]) res.send(500);
+  imageDB.get(p[1],function(data) {
+    res.send(makeTemplate("view",{image: _.defaults(data, defaultImage), title: "Boorushy v.2"},p[2]));
+  });
+});
+function listImages(res,images1,p,p2) {
+  var start = parseInt(p) || 0;
+  var isRaw = p2 || false;
+  var noHeader = false;
+  if(parseInt(p)==NaN) { start = 0; isRaw = p; }
+  console.log("listImages: " + start + " " + isRaw);
+  imageDB.range(images1,start,config.pageSize,function(images2) {
+    var conf = {images: images2, position: start, maxpos: images1.length, title: "Boorushy v.2"};
+    var imagesLi = makeRawTemplate("images-li",conf,"raw",true);
+    conf.imagesLi = imagesLi;
+    if(isRaw == "append") res.send(conf.imagesLi);
+    else res.send(makeTemplate("images",conf,isRaw,noHeader));
+  });
+}
+app.use("/",function(req,res) {
+  var p = qs.unescape(req.path).split("/");
+  console.log("Request: " + JSON.stringify(p)); 
+  if(p.length>2 && (p[1] == "tag" || p[1] == "author" || p[1] == "uploader")) imageDB.imagesBy(p[1],p[2],function(images) { listImages(res,images,p[3],p[4]); });
+  else imageDB.images(function(images) { listImages(res,images,p[1],p[2]); });
+});
+
+imageDB.connect();
+app.listen(config.port);
+console.log("Working on port " + config.port);
+
+program.version("0.0.1");
+program.command("add [file]")
+       .description("add file to db")
+       .action(function(filename){
+         addImage(fs.readFileSync(filename),fileExt(filename),{tags: ["test","less tests"],filename: filename});
+       });
+
+program.parse(process.argv);
---
+++ imagedb.js
@@ -0,0 +1,113 @@
+var ImageDB = {}
+  , redis = require("redis")
+  , _ = require("underscore")
+  , JSON = require("JSON2")
+  , async = require("async");
+
+var client = null;
+
+ImageDB.connect = function(port,host,options) {
+  client = redis.createClient(port || null,host || null,options || {});
+}
+
+ImageDB.get = function(id,callback) {
+  client.get("data:"+id,function(err,out) {
+    if(err) throw err;
+    callback(JSON.parse(out));
+  });
+}
+ImageDB.getWithError = function(id,callback) {
+  client.get("data:"+id,function(err,out) {
+    callback(err,JSON.parse(out));
+  });
+}
+
+ImageDB.exists = function(id,cb) { return client.exists(client,"data:"+id,cb); }
+
+ImageDB.addField = function(id,key,val) {
+  client.sadd(key+"s",val);
+  client.sadd(key+":"+val,id);
+}
+ImageDB.delField = function(id,key,val) {
+  client.srem(key+":"+val,id);
+  client.scard(key+":"+val, function(err,m) {
+    if(err) throw err;
+    if(m>0) return;
+    client.del(key+":"+val);
+    client.srem(key+"s",val);
+  });
+}
+
+ImageDB.tags = function(callback) {
+  client.smembers("tags", function(err,m) {
+    if(err) throw err;
+    if(_.isNull(m)) callback(new Array());
+    else callback(m.sort());
+  });
+}
+ImageDB.images = function(callback) {
+  client.smembers("images", function(err,m){
+    if(err) throw err;
+    if(_.isNull(m)) callback(new Array());
+    else callback(_.sortBy(m,function(num){ return 0-num; }));
+  });
+}
+ImageDB.imagesBy = function(key,val,callback) {
+  client.smembers(key+":"+val, function(err,m){
+    if(err) throw err;
+    if(_.isNull(m)) callback(new Array());
+    else callback(_.sortBy(m,function(num){ return 0-num; }));
+  });
+}
+ImageDB.range = function(arr2,s,l,callback) {
+  var out = new Array();
+  var arr = arr2;
+  if(_.isNumber(arr2)) arr = new Array(arr2);
+  arr = _.first(_.rest(arr,s),l);
+  async.map(arr,this.getWithError,function(err, results) {
+    if(err) throw err;
+    callback(results);
+  });
+}
+
+ImageDB.count = function(name,callback) {
+  client.setnx("counter:"+name,0);
+  client.incr("counter:"+name,callback);
+}
+ImageDB.uncount = function(name,callback) {
+  client.setnx("counter:"+name,0);
+  client.decr("counter:"+name,callback);
+}
+ImageDB.add = function(data,callback) {
+  this.count("imageId",function(count) {
+    this.set(count,data);
+    if(_.isFunction(callback))
+      callback(count);
+  });
+}
+ImageDB.set = function(id,data) {
+  data.id = id;
+  client.set("data:"+id,JSON.stringify(data));
+  client.sadd("images",id);
+  var t = this;
+  _.each(data.tags,function(tag) { t.addField(id,"tag",tag); });
+  if(_.isString(data.author))
+    this.addField(id,"author",data.author);
+  if(_.isString(data.uploader))
+    this.addField(id,"uploader",data.uploader);
+}
+
+ImageDB.unset = function(id) {
+  this.get(id,function(data) {
+    client.del("data:"+id);
+    client.srem("images",id);
+    var t = this;
+    _.each(data.tags,function(tag) { t.delField(id,"tag",tag); });
+    if(_.isString(data.author))
+      this.delField(id,"author",data.author);  
+    if(_.isString(data.uploader))
+      this.delField(id,"uploader",data.uploader);
+  });
+}
+
+exports.ImageDB = ImageDB;
